f9fe04401802202b042c69c3ca1337a619a811b0
Mohammad-Basheer <34026154+Mohammad-Basheer@users.noreply.github.com>
2024-11-09 13:52:49 +0300

    Update spacex_dash_app.py

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -1,3 +1,18 @@
+# Import required libraries
+import pandas as pd
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import plotly.express as px
+
+# Read the airline data into pandas dataframe
+spacex_df = pd.read_csv("spacex_launch_dash.csv")
+max_payload = spacex_df['Payload Mass (kg)'].max()
+min_payload = spacex_df['Payload Mass (kg)'].min()
+
+# Create a dash application
+app = dash.Dash(__name__)
 # Create an app layout
 app.layout = html.Div(children=[
     html.H1('SpaceX Launch Records Dashboard',
